5b66d2383235791eccd2536bc8f3d33d260d7c64
Hagen <73616486+Hagen-Fulford@users.noreply.github.com>
2022-09-15 14:13:15 +0700

    adding style and content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Account from './components/Account';
+import {Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-      <h1>Testing</h1>
+      <h1 className=" text- center text-3xl font bold">
+        Firebase Auth & Context
+        </h1>
+        <Routes>
+          <Route path='/' element={<Signin/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+          <Route path='/account' element={<Account
+          />}/>
+        </Routes>
     </div>
+  
   );
 }
 
--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
 const Account = () => {
   return (
-    <div>Account</div>
-  )
-}
+    <div className='max-w-[600px] mx-auto my-16 p-4'>
+      <h1 className='text-2xl font-bold py-4'>Account</h1>
+      <p>User Email</p>
 
-export default Account
+      <button className='border px-6 py-2 my-4'>
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Account;
