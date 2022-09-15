80db04ab4937ca511108b12bb6c72a78bd6cae45
Hagen <73616486+Hagen-Fulford@users.noreply.github.com>
2022-09-15 14:22:38 +0700

    setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'process.env.REACT_APP_FIRST_API',
+    authDomain: "fir-auth-33631.firebaseapp.com",
+    projectId: "fir-auth-33631",
+    storageBucket: "fir-auth-33631.appspot.com",
+    messagingSenderId: "194788678463",
+    appId: "1:194788678463:web:acf53dcc0c47509d2c118a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+
+export default app
